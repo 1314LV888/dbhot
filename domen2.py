6191bc9da888fae9cd49cec00babf65534921055
Cherish <482926218@qq.com>
2021-12-21 21:58:08 +0800

    v1

---
+++ domen2.py
@@ -0,0 +1,13 @@
+#转义字符
+print("hello\nworld")    #\ +转义功能的首字母  n→newline的首字母表示换行
+print("hello\tworld")
+print("helloooo\tworld")
+print("hello\rworld")  #world将hello进行了覆盖
+print("hello\bworld")  #\b是退一个格 将o退没了
+
+
+print("http;\\\\www.baidu.com")
+print ("老师说；大家好")
+
+#原字符，不希望字符串中的转义字符起作用，就使用原字符，就是在字符串中加上r或R
+print(r'hello\nworld')
